cf733d84caa27901da69c83a8fba68813ecb01cd
LOGANATHAN D <59384550+logan0501@users.noreply.github.com>
2021-06-16 23:01:50 +0530

    finally added files

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+     body: FireMap(),
+    );
+    }
+  }
+class FireMap extends StatefulWidget {
+  @override
+  _FireMapState createState() => _FireMapState();
+}
+
+class _FireMapState extends State<FireMap> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        GoogleMap(initialCameraPosition: CameraPosition(
+          target: LatLng(13.131613,79.242843),
+          zoom: 10
+        ))
+      ],
+    );
+  }
+}
+
